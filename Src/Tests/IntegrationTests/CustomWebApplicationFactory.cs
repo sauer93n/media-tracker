1319068ac3b96728caa54a96a2bd722e63c2ef98
0xy93n <sauer93n@gmail.com>
2025-11-26 21:59:44 +0300

    refactor: Remove unused using directives in CustomWebApplicationFactory

--- Src/Tests/IntegrationTests/CustomWebApplicationFactory.cs
+++ Src/Tests/IntegrationTests/CustomWebApplicationFactory.cs
@@ -1,8 +1,4 @@
-using Infrastructure.Context;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.AspNetCore.TestHost;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using Testcontainers.PostgreSql;
 using Xunit;
 
